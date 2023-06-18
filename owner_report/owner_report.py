60d635a87877fcb87a8f2224415cfaf056bedc68
dithpri <dithpri@gmail.com>
2023-06-18 20:53:16 +0200

    Update to support new sans

--- owner_report/owner_report.py
+++ owner_report/owner_report.py
@@ -16,9 +16,7 @@ def eprint(*args, **kwargs):
 
 
 try:
-    from sans.api import Api
-    from sans.utils import pretty_string
-    from sans.errors import NotFound
+    import sans
 except ImportError:
     eprint(
         """You need `sans` to run this script!
@@ -41,14 +39,7 @@ def canonical_nation_name(name):
     return name.lower().replace(" ", "_")
 
 
-async def ratelimit():
-    while xra := Api.xra:
-        xra = xra - datetime.datetime.now().timestamp()
-        eprint(f"Rate limit reached: sleeping {int(xra)} seconds...")
-        await asyncio.sleep(xra)
-
-
-current_season = 2
+current_season = 3
 
 
 async def main():
@@ -65,7 +56,7 @@ async def main():
             eprint("That's not a number!")
     while not username:
         username = input("Please enter your nation name: ")
-    Api.agent = f"Owner Report (dev. dithpri/Racoda) (in use by {username})"
+    sans.set_agent(f"Owner Report (dev. dithpri/Racoda) (in use by {username})")
     output_file = open(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S.tsv"), "x")
     cards = set()
     if mode in [2, 3]:
@@ -88,36 +79,47 @@ async def main():
                 if season:
                     cards.add((id, season))
                 else:
-                    for s in range(current_season + 1):
+                    for s in range(1, current_season + 1):
                         cards.add((id, s))
+    client = sans.AsyncClient()
     if mode in [1, 3]:
         with open("puppets.txt", "r") as input_file:
             puppets = filter(None, input_file.read().splitlines())
         for puppet in puppets:
-            await ratelimit()
-            result = await Api("cards deck", nationname=canonical_nation_name(puppet))
-            try:
-                for card in result.DECK.CARD:
-                    cards.add((card.CARDID.pyval, card.SEASON.pyval))
-            except AttributeError:
+            result = (
+                await client.get(
+                    sans.World(
+                        "cards deck",
+                        nationname=canonical_nation_name(puppet),
+                    )
+                )
+            ).xml
+            if result.find("./DECK/CARD") is None:
                 eprint(f"{puppet} does not exist or has an empty deck.")
                 continue
+            for card in result.find("./DECK").iter("CARD"):
+                cards.add(
+                    (
+                        int(card.find("./CARDID").text),
+                        int(card.find("./SEASON").text),
+                    )
+                )
     output_file.write("ID\tSEASON\tNUMBER OF OWNERS\tNUMBER OF COPIES\tOWNERS:COPIES\n")
     for card in cards:
         id, season = card
         owners_dict = defaultdict(int)
         num_owners = 0
         num_copies = 0
-        await ratelimit()
-        result = await Api("card owners", cardid=id, season=season)
-        try:
-            for owner in result.OWNERS.OWNER:
-                num_copies += 1
-                owners_dict[owner.text] += 1
-        except AttributeError:
-            if result.find("OWNERS") == None:
-                eprint(f"Card {id} season {season} does not exist.")
-                continue
+        result = (
+            await client.get(sans.World("card owners", cardid=id, season=season))
+        ).xml
+        owners = result.find("./OWNERS")
+        if owners is None:
+            eprint(f"Card {id} season {season} does not exist.")
+            continue
+        for owner in owners.iter("OWNER"):
+            num_copies += 1
+            owners_dict[owner.text] += 1
         owners = owners_dict.keys()
         num_owners = len(owners)
         owners_copies = ",".join(
@@ -131,6 +133,7 @@ async def main():
         output_file.write(
             f"{id}\t{season}\t{num_owners}\t{num_copies}\t{owners_copies}\n"
         )
+    await client.aclose()
 
 
 if __name__ == "__main__":
