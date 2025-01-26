4af41941ac3281461ca6ee7d9f0b4ff6deecf282
dithpri <dithpri@gmail.com>
2025-01-26 23:00:17 +0100

    S4 support for Better Card Page Titles
    Closes #13
    
    Co-authored-by: Sitethief <sitethief@gmail.com>

--- userscripts/miscellaneous/Better Card Page Titles.user.js
+++ userscripts/miscellaneous/Better Card Page Titles.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Card Page Titles
 // @namespace    dithpri.RCES
-// @version      0.2
+// @version      0.2.1
 // @description  Descriptive titles for card pages, for easier browsing history searching.
 // @author       dithpri
 // @downloadURL  https://github.com/dithpri/RCES/raw/master/userscripts/miscellaneous/Better%20Card%20Page%20Titles.user.js
@@ -11,6 +11,13 @@
 // ==/UserScript==
 
 const capitalize = (x) => x[0].toUpperCase() + x.slice(1);
+
+const season = () =>
+	document.querySelector(".deckcard-season-list-card-selected .minicard-season-number")?.textContent ||
+	"Unknown season";
+
+const name = () =>
+	document.querySelector(".deckcard .nnameblock .nname, .s4-card-wrapper a.title")?.textContent || "Unknown card";
 
 (function () {
 	"use strict";
@@ -33,9 +40,7 @@ const capitalize = (x) => x[0].toUpperCase() + x.slice(1);
 	} else if (urlParams.has("show_trades")) {
 		titleEx += ` | ${capitalize(urlParams.get("show_trades"))}`;
 	} else if (urlParams.has("card")) {
-		titleEx = ` | ${document.querySelector(".deckcard .nnameblock .nname").textContent} (S${
-			document.querySelector(".deckcard-season-list-card-selected .minicard-season-number").textContent
-		}) ${titleEx}`;
+		titleEx = ` | ${name()} (S${season()}) ${titleEx}`;
 		// ^ Prepend to titleEx, because it already contains the h2 title - which should go at the end.
 
 		if (urlParams.has("finds_history")) {
